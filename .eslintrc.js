d91ac96163363669b0eb8dab88c4051b7160ede5
rafaeldellaquila <rafael@dellaquila.dev>
2022-01-19 17:02:31 -0300

    :sparkles: start project and new configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,27 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint",
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-};
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        '@vue/prettier',
+    ],
+
+    parserOptions: {
+        ecmaVersion: 2020,
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'max-len': ['error', { code: 150, ignoreUrls: true }],
+        'prettier/prettier': [
+            'error',
+            {},
+            {
+                usePrettierrc: true,
+            },
+        ],
+    },
+}
