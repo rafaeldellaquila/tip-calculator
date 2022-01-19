35efa7c1d16411ec77943d6dd06d7ecefbd2cdad
rafaeldellaquila <rafael@dellaquila.dev>
2022-01-19 17:40:10 -0300

    :sparkles: button new component and input component

---
+++ src/components/Button/ButtonComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <button type="button" :value="this.value">{{ this.title }}</button>
+</template>
+
+<script>
+export default {
+    name: 'InputComponent',
+
+    props: {
+        value: Number,
+        title: String,
+    },
+}
+</script>
+
+<style scoped lang="scss">
+@import '../../styles/theme.scss';
+</style>
--- src/components/CardComponents/InputCardComponent.vue
+++ src/components/CardComponents/InputCardComponent.vue
@@ -2,21 +2,62 @@
     <legend>Bill</legend>
     <input-component name="bill" />
     <legend>Select Tip %</legend>
+    <button-component
+        v-for="(value, index) in tipValues"
+        :value="value.value"
+        :title="value.name"
+        :key="index"
+    />
     <legend>Number of People</legend>
     <input-component name="people" />
 </template>
 
 <script>
+import ButtonComponent from '../Button/ButtonComponent.vue'
 import InputComponent from '../Input/InputComponent.vue'
+
+const tipValues = [
+    {
+        name: '5%',
+        value: 5,
+    },
+    {
+        name: '10%',
+        value: 10,
+    },
+    {
+        name: '15%',
+        value: 15,
+    },
+    {
+        name: '25%',
+        value: 25,
+    },
+    {
+        name: '50%',
+        value: 50,
+    },
+    {
+        name: 'Custom',
+        value: null,
+    },
+]
+
 export default {
-    components: { InputComponent },
+    components: { InputComponent, ButtonComponent },
     name: 'InputCardComponent',
+
+    setup() {
+        return {
+            ButtonComponent: tipValues,
+        }
+    },
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 @import '../../styles/theme.scss';
+
 legend {
     font: $font-700;
     text-transform: capitalize;
